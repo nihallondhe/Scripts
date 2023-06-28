2e91ea1d430e18bdfa0073bf02d6a56f4e5cee5c
Nihal Londhe <79393628+nihallondhe@users.noreply.github.com>
2023-06-28 12:02:04 +0530

    Update terraform.sh
    update and add aws-cli

--- terraform.sh
+++ terraform.sh
@@ -30,4 +30,10 @@ sudo apt update
 
 sudo apt-get install terraform
 
+#------------installing aws-cli----------------
+apt-get install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 fi
